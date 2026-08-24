475647e5e9b36335b40497788d0062eed42aff3a
Rehemu Wulugebige <159103811+rehemuWulugebige@users.noreply.github.com>
2026-08-23 23:46:36 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = {}
+        for (const s of strs) {
+            const count = new Array(26).fill(0)
+            for (const c of s) {
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            }
+            const key = count.join(',')
+            if (!map[key]) {
+                map[key] = []
+            }
+            map[key].push(s)
+        }
+        return Object.values(map)
+    }
+}
