7e47cc0afe6dca5c8461c461f47f2d89bb630bba
Rehemu Wulugebige <159103811+rehemuWulugebige@users.noreply.github.com>
2026-08-31 23:23:01 -0400

    Add: python-custom-heap - submission-0

---
+++ Python Coding Interviews/python-custom-heap/submission-0.py
@@ -0,0 +1,24 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    heap = []
+
+    for num in nums: 
+        heapq.heappush(heap, (-num, num))
+
+    res = []
+
+    while heap: 
+        pop = heapq.heappop(heap)
+        n = pop[1]
+        res.append(n)
+
+    return res
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
